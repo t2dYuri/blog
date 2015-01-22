0e6a0c080bf1fb0666292ea272fc2c47358d4e07
Trend <t2d.yuri@gmail.com>
2015-01-22 20:33:08 +0200

    finished with users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,13 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
 
   def index
-    @users = User.paginate(page: params[:users_page], :per_page => 10)
+    @users = User.where(activated: true).paginate(page: params[:users_page], :per_page => 10)
     @articles = Article.paginate(page: params[:articles_page], :per_page => 10)
   end
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated
   end
 
   def new
